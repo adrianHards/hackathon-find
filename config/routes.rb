66404e82431c5d7ca0e2aee413bf0ffe1964f00a
Adrian <github@ahards.com>
2022-10-22 20:31:23 +0100

    fetch with params

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "patients#upload"
-  
+
   resources :patients, only: [:create, :show, :index]
   get "/confirmation", to: "patients#confirmation", as: :confirmation
 
-  post "/send_cloudinary", to: "patients#send_cloudinary", as: :send_cloudinary
+  post "/cloudinary", to: "patients#cloudinary", as: :cloudinary
 end
