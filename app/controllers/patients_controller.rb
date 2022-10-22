302231155e1fe65fe8fe6ee51f097043534fa4c6
BsBou <borhan.boulandier@gmail.com>
2022-10-22 12:24:15 +0100

    Create patients method with strong params

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,26 @@
 class PatientsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def index
+    
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      render json: @patient.as_json, status: :created
+    else
+      render json: @patient.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    
+  
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:first_name, :last_name, :phone_numbers, :location, :details)
+  end
 end
