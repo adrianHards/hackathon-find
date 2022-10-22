8e4bb2b30511767a13d28d826eec2789f4d69b8e
Luca Di Vittorio <luca.div@hotmail.com>
2022-10-22 11:46:42 +0100

    added cloudinary to the model patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,5 @@
 class PatientsController < ApplicationController
+  def patients_params
+    params.require(:patient).permit(:first_name, :last_name, :phone_numbers [], :location, :details, :photo)
+  end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,3 @@
 class Patient < ApplicationRecord
+  has_one_attached :photo
 end
