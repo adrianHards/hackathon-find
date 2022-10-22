23f2d394be31e0f2e1b21fe2c4821a9e3210d6fc
Adrian <github@ahards.com>
2022-10-22 12:20:42 +0100

    finalise routes and associated view pages

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,15 @@
 class PatientsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [:upload, :confirmation]
+
   def upload
+  end
+
+  def confirmation
+  end
+
+  def index
+  end
+
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "patients#upload"
 
+  resources :patients, only: [:home, :show, :index]
+  get "/confirmation", to: "patients#confirmation", as: :confirmation
 
 end
