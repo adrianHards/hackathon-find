f5e2d4fdb5ae9484860767409927df543571ca7d
Adrian Hards <github@ahards.com>
2022-10-22 12:31:39 +0100

    Update patients_controller.rb

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,7 +3,6 @@ class PatientsController < ApplicationController
   skip_before_action :verify_authenticity_token
 # skip_before_action :authenticate_user!, only: [:upload, :confirmation]
 
-
   def upload
   end
 
