203c86bcc4d0fefd4c2e601296950df1414fc860
Adrian <github@ahards.com>
2022-10-23 12:20:09 +0100

    model-fix

--- db/migrate/20221022100909_create_patients.rb
+++ db/migrate/20221022100909_create_patients.rb
@@ -2,7 +2,7 @@ class CreatePatients < ActiveRecord::Migration[7.0]
   def change
     create_table :patients do |t|
       t.string :name, array: true, default: []
-      t.string :phone_numbers, array: true, default: []
+      t.string :phone_numbers
       t.string :location
       t.text :details
 
